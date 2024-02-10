3c45847f7b525a8302f3cfb2e145c4630bbf3603
Alex02354 <a.abrahamek@gmail.com>
2024-02-11 00:08:59 +0100

    Formatting

--- src/graphqlClient.js
+++ src/graphqlClient.js
@@ -1,14 +1,14 @@
 import { GraphQLClient } from "graphql-request";
 
-const url = 'https://doylestown.stepzen.net/api/vociferous-elk/__graphql';
+const url = "https://doylestown.stepzen.net/api/vociferous-elk/__graphql";
 
 const apiKey = process.env.EXPO_PUBLIC_GRAPHQL_API_KEY;
 
 const client = new GraphQLClient(url, {
-    headers:{
-        Authorization:
-         'apikey doylestown::stepzen.io+1000::3a0611000eba6d01fad59ad708da5a198a85169b0162f2b23927b8668ed7649b'
-        },
-    });
+  headers: {
+    Authorization:
+      "apikey doylestown::stepzen.io+1000::3a0611000eba6d01fad59ad708da5a198a85169b0162f2b23927b8668ed7649b",
+  },
+});
 
 export default client;
